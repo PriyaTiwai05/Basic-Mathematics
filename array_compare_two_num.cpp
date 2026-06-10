8c013249aab94e2767f21221f6ca4f447c893f39
Priya Raj <priyati162505@gmail.com>
2026-06-10 08:53:25 +0530

    Compare 2 num

--- array_compare_two_num.cpp
+++ array_compare_two_num.cpp
@@ -5,11 +5,13 @@ int main(){
     int num[]={3,7,55,89,-2,66,1};
     int size = 7;
     int smallest = INT8_MAX;
+    int largest = INT8_MIN;
     for(int i = 0;i<=7;i++){
-        if(num[i]<smallest){
-            smallest = num[i];
+        smallest = min(num[i],smallest);
+        smallest = max(num[i],largest);
         }
-    }
+    
     cout<<"smallest "<<smallest;
+    cout<<"largest "<<largest;
     return 0;
 }
